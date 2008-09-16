c1c489db2e76d5a6d0d9b10c86fb6d848721ac0f
Christian Fowler <spider@viovio.com>
2008-09-16 01:23:10 +0000

    recent language export as of 21-AUG-2008

--- lang/en-uk/language.php
+++ lang/en-uk/language.php
@@ -1,50 +1,20 @@
-<?php // -*- coding:utf-8 -*-
-// parameters:
-// lang=xx    : only tranlates language 'xx',
-//              if not given all languages are translated
-// comments   : generate all comments (equal to close&module)
-// close      : look for similar strings that are allready translated and
-//              generate a commet if a 'match' is made
-// module     : generate comments that describes in which .php and/or .tpl
-//              module(s) a certain string was found (useful for checking
-//              translations in context)
-// patch      : looks for the file 'language.patch' in the same directory
-//              as the corresponding language.php and overrides any strings
-//              in language.php - good if a user does not agree with
-//              some translations or if only changes are sent to the maintaner
-// spelling   : generates a file 'spellcheck_me.txt' that contains the
-//              words used in the translation.It is then easy to check this
-//              file for spelling errors (corrections must be done in
- //              'language.php, however)
-// groupwrite : Sets the generated files permissions to allow the generated
-//              language.php also be group writable. This is good for
-//              translators if they do not have root access to tiki but
-//              are in the same group as the webserver. Please remember
-//              to have write access removed when translation is finished
-//              for security reasons. (Run script again without this
-//              parameter)
-// Examples:
-// http://www.neonchart.com/get_strings.php?lang=sv
-// Will translate langauage 'sv' and (almost) avoiding comment generation
-
-// http://www.neonchart.com/get_strings.php?lang=sv&comments
-// Will translate langauage 'sv' and generate all possible comments.
-// This is the most usefull mode when working on a translation.
-
-// http://www.neonchart.com/get_strings.php?lang=sv&nohelp&nosections
-// These options will only provide the minimal amout of comments.
-// Usefull mode when preparing a translation for distribution.
-
-// http://www.neonchart.com/get_strings.php?nohelp&nosections
-// Prepare all languages for release 
-
-
+<?php
+// This language is the  translation of bitweaver and
+// it was exported from the bitweaver database on 2008-08-25 08:07
 $lang=Array(
-"the background color, use #rrvvbb color types.\n" => "the background colour, use #rrvvbb colour types.\n",
-"color of the border\n" => "colour of the border\n",
-"background color of the node\n" => "background colour of the node\n",
-"color for links (called edges here)\n" => "colour for links (called edges here)\n",
-"Colored text" => "Coloured text",
-"Will display using the indicated HTML color" => "Will display using the indicated HTML colour",
+'background color of the node
+' => 'background colour of the node
+',
+'colored text' => 'Coloured text',
+'color for links (called edges here)
+' => 'colour for links (called edges here)
+',
+'color of the border
+' => 'colour of the border
+',
+'the background color, use #rrvvbb color types.
+' => 'the background colour, use #rrvvbb colour types.
+',
+'Will display using the indicated HTML color' => 'Will display using the indicated HTML colour',
 );
 ?>
